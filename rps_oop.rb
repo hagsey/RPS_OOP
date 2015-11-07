190cd9540176c89293407c362e552172e090be98
Eric Hagemann <hagemann.eric@gmail.com>
2015-11-07 14:34:24 -0500

    Finished RPS_OOP game

--- rps_oop.rb
+++ rps_oop.rb
@@ -1,38 +1,92 @@
+class Player
+  attr_accessor :choice
+  attr_reader :name
 
+  def initialize(n)
+    @name = n
+  end
 
-puts "Play Rock, Paper, Scissors!"
-
-def display_winning_message(winning_choice)
-  case winning_choice
-  when 'P'
-    puts "Paper covers rock!"
-  when 'S'
-    puts "Scissors cuts paper!"
-  when 'R'
-    puts "Rock smashes scissors!"
+  def to_s
+    "#{name} chose #{Game::HANDS.fetch(choice)}"
   end
 end
 
-loop do
-  begin
-    puts "Choose one: (R/P/S)"
-    player_choice = gets.chomp
-  end until %(R P S).include?(player_choice)
-  computer_choice = %w(R P S).sample
-  puts "You picked #{player_choice} and computer picked #{computer_choice}."
-  result = player_choice + computer_choice
-
-  case result
-  when 'RR', 'SS', 'PP'
-    puts "It's a tie."
-  when 'RS', 'PR', 'SP'
-    display_winning_message(player_choice)
-    puts "You won!"
-  else
-    display_winning_message(computer_choice)
-    puts "You lost!"
+class Human < Player 
+ 
+  def initialize
+    puts "What is your name?"
+    human_name = gets.chomp
+    @name = human_name
   end
-  puts "Play again? (Y/N)"
-  play_again = gets.chomp.upcase
-  break unless play_again == 'Y'
+
+  def select_hand
+    begin
+      puts "Choose one: (R/P/S)"
+      c = gets.chomp.upcase
+    end until Game::HANDS.keys.include?(c)
+   self.choice = c
+  end
 end
+
+class Computer < Player
+  def select_hand
+    self.choice = Game::HANDS.keys.sample 
+  end
+end
+
+
+class Game
+  HANDS = {'R' => 'Rock', 'P' => 'Paper', 'S' => 'Scissors'}        
+
+  attr_reader :player, :computer 
+
+  def initialize
+    @player = Human.new
+    @computer = Computer.new("iMac")
+  end
+
+  def display_winning_message(winning_choice)
+    case winning_choice
+    when 'P'
+      puts "Paper covers rock!"
+    when 'S'
+      puts "Scissors cuts paper!"
+    when 'R'
+      puts "Rock smashes scissors!"
+    end
+  end
+
+  def compare_hands
+    result = player.choice + computer.choice
+    case result
+    when 'RR', 'SS', 'PP'
+      puts "It's a tie."
+    when 'RS', 'PR', 'SP'
+      display_winning_message(player.choice)
+      puts "#{player.name} won!"
+    else
+      display_winning_message(computer.choice)
+      puts "#{player.name} lost :("
+    end
+  end
+
+  def play
+    sleep 1
+    puts "Welcome #{player.name}, let's play Rock, Paper, Scissors!"
+    loop do
+      player.select_hand
+      computer.select_hand
+      puts player
+      sleep 1
+      puts computer
+      sleep 1
+      compare_hands
+      sleep 1
+      puts "Play again? (Y/N)"
+      play_again = gets.chomp.upcase
+      break unless play_again == 'Y'
+    end
+  end
+end
+
+game = Game.new.play
